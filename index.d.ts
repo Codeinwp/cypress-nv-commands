1f0edef0a7a239588d0c89cb05cadb433037cf6a
Augusto Silva <augusto@themeisle.com>
2021-07-09 09:46:19 -0300

    style: fix eslint errors

--- index.d.ts
+++ index.d.ts
@@ -1,197 +1,197 @@
 interface HTMLElement {
-	href;
+  href;
 }
 declare namespace Cypress {
-	interface WP {
-		customize;
-		data: {
-			select;
-			dispatch;
-		};
-	}
-	interface Window {
-		wp: WP;
-		appReady: boolean;
-	}
+  interface WP {
+    customize;
+    data: {
+      select;
+      dispatch;
+    };
+  }
+  interface Window {
+    wp: WP;
+    appReady: boolean;
+  }
 
-	interface Chainable {
-		get<K extends keyof HTMLElementTagNameMap>(
-			selector: K | JQuery<HTMLElement>,
-			options?: Partial<Loggable & Timeoutable & Withinable & Shadow>,
-		): Chainable<JQuery<HTMLElementTagNameMap[K]>>;
+  interface Chainable {
+    get<K extends keyof HTMLElementTagNameMap>(
+      selector: K | JQuery<HTMLElement>,
+      options?: Partial<Loggable & Timeoutable & Withinable & Shadow>,
+    ): Chainable<JQuery<HTMLElementTagNameMap[K]>>;
 
-		/**
-		 * Login through request
-		 *
-		 * @param nextRoute - the url you want to visit after logging in
-		 * @example
-		 *   cy.loginWithRequest('/shop')
-		 */
-		loginWithRequest(nextRoute?: string): void;
+    /**
+     * Login through request
+     *
+     * @param nextRoute - the url you want to visit after logging in
+     * @example
+     *   cy.loginWithRequest('/shop')
+     */
+    loginWithRequest(nextRoute?: string): void;
 
-		/**
-		 * Login through UI once and verify if the user is already logged in
-		 *
-		 * @param nextRoute - the url you want to visit after logging in
-		 * @example
-		 *   cy.login('/shop')
-		 */
-		login(nextRoute?: string): void;
+    /**
+     * Login through UI once and verify if the user is already logged in
+     *
+     * @param nextRoute - the url you want to visit after logging in
+     * @example
+     *   cy.login('/shop')
+     */
+    login(nextRoute?: string): void;
 
-		/**
-		 * Clear the welcome message
-		 *
-		 * @example cy.clearWelcome()
-		 */
-		clearWelcome(): void;
+    /**
+     * Clear the welcome message
+     *
+     * @example cy.clearWelcome()
+     */
+    clearWelcome(): void;
 
-		/**
-		 * Insert a new post
-		 *
-		 * @param title
-		 * @param content
-		 * @param type
-		 * @param featured
-		 * @param tags
-		 * @example
-		 *  cy.insertPost('Title', 'Content', 'post', false, false)
-		 */
-		insertPost(
-			title: string,
-			content: string,
-			type: string,
-			featured?: boolean,
-			tags?: boolean,
-		): void;
+    /**
+     * Insert a new post
+     *
+     * @param title
+     * @param content
+     * @param type
+     * @param featured
+     * @param tags
+     * @example
+     *  cy.insertPost('Title', 'Content', 'post', false, false)
+     */
+    insertPost(
+      title: string,
+      content: string,
+      type: string,
+      featured?: boolean,
+      tags?: boolean,
+    ): void;
 
-		/**
-		 * Inserts a new post by request
-		 *
-		 * @param title
-		 * @param content
-		 * @param type
-		 * @param featured the id of the image you want to feature
-		 */
-		insertPostWithRequest(title: string, content: string, type: string, featured?: number);
+    /**
+     * Inserts a new post by request
+     *
+     * @param title
+     * @param content
+     * @param type
+     * @param featured the id of the image you want to feature
+     */
+    insertPostWithRequest(title: string, content: string, type: string, featured?: number);
 
-		/**
-		 * Click on update button on a published post
-		 *
-		 * @example
-		 *  cy.updatePost()
-		 */
-		updatePost(): void;
+    /**
+     * Click on update button on a published post
+     *
+     * @example
+     *  cy.updatePost()
+     */
+    updatePost(): void;
 
-		/**
-		 * Gets the selector from Customizer Control
-		 *
-		 * @param slug
-		 * @example
-		 *  cy.getCustomizerControl('neve_global')
-		 */
-		getCustomizerControl(slug: string): Chainable;
+    /**
+     * Gets the selector from Customizer Control
+     *
+     * @param slug
+     * @example
+     *  cy.getCustomizerControl('neve_global')
+     */
+    getCustomizerControl(slug: string): Chainable;
 
-		/**
-		 * Sets Customizer Settings using UI
-		 *
-		 * @param to
-		 * @example
-		 *  cy.setCustomizeSettings({ hfg_header_layout: withSearch })
-		 */
-		setCustomizeSettings(to: unknown): Chainable;
+    /**
+     * Sets Customizer Settings using UI
+     *
+     * @param to
+     * @example
+     *  cy.setCustomizeSettings({ hfg_header_layout: withSearch })
+     */
+    setCustomizeSettings(to: unknown): Chainable;
 
-		/**
-		 * Navigates to the customizer UI
-		 *
-		 * @example
-		 *  cy.goToCustomizer()
-		 */
-		goToCustomizer(): void;
+    /**
+     * Navigates to the customizer UI
+     *
+     * @example
+     *  cy.goToCustomizer()
+     */
+    goToCustomizer(): void;
 
-		/**
-		 * Alias POST route to /wp-admin/admin-ajax.php as customizerSave
-		 *
-		 * @example
-		 *  cy.aliasRestRoutes()
-		 */
-		aliasRestRoutes(): void;
+    /**
+     * Alias POST route to /wp-admin/admin-ajax.php as customizerSave
+     *
+     * @example
+     *  cy.aliasRestRoutes()
+     */
+    aliasRestRoutes(): void;
 
-		/**
-		 * Toggle elements on or off
-		 *
-		 * @param show
-		 * @example
-		 *  cy.toggleElements(false)
-		 */
-		toggleElements(show: boolean): void;
+    /**
+     * Toggle elements on or off
+     *
+     * @param show
+     * @example
+     *  cy.toggleElements(false)
+     */
+    toggleElements(show: boolean): void;
 
-		/**
-		 * Selector for a control
-		 *
-		 * @param control
-		 * @example
-		 *  cy.getControl('neve_sidebar')
-		 */
-		getControl(control: string): Chainable;
+    /**
+     * Selector for a control
+     *
+     * @param control
+     * @example
+     *  cy.getControl('neve_sidebar')
+     */
+    getControl(control: string): Chainable;
 
-		/**
-		 * Activates a checkbox
-		 *
-		 * @param checkboxSelector
-		 * @param checkboxText
-		 * @example
-		 *	cy.activateCheckbox('#neve', 'check text')
-		 */
-		activateCheckbox(checkboxSelector: string, checkboxText: string): void;
+    /**
+     * Activates a checkbox
+     *
+     * @param checkboxSelector
+     * @param checkboxText
+     * @example
+     *	cy.activateCheckbox('#neve', 'check text')
+     */
+    activateCheckbox(checkboxSelector: string, checkboxText: string): void;
 
-		/**
-		 * Opens sidebar on Neve Options
-		 *
-		 * @example
-		 *  cy.openNeveSidebar()
-		 */
-		openNeveSidebar(): Chainable;
+    /**
+     * Opens sidebar on Neve Options
+     *
+     * @example
+     *  cy.openNeveSidebar()
+     */
+    openNeveSidebar(): Chainable;
 
-		/**
-		 * Activates Classic editor plugin
-		 *
-		 * @example
-		 *  cy.activateClassicEditorPlugin()
-		 */
-		activateClassicEditorPlugin(): void;
+    /**
+     * Activates Classic editor plugin
+     *
+     * @example
+     *  cy.activateClassicEditorPlugin()
+     */
+    activateClassicEditorPlugin(): void;
 
-		/**
-		 * Deactivates Classic editor plugin
-		 *
-		 * @example
-		 *  cy.deactivateClassicEditorPlugin()
-		 */
-		deactivateClassicEditorPlugin(): void;
+    /**
+     * Deactivates Classic editor plugin
+     *
+     * @example
+     *  cy.deactivateClassicEditorPlugin()
+     */
+    deactivateClassicEditorPlugin(): void;
 
-		/**
-		 * Get a JWT token to send request to Wordpress REST API
-		 *
-		 * @example cy.getJWT()
-		 */
-		getJWT();
+    /**
+     * Get a JWT token to send request to Wordpress REST API
+     *
+     * @example cy.getJWT()
+     */
+    getJWT();
 
-		/**
-		 * Updates a post or page by request to WP Rest API.
-		 * Needs a valid JWT set on local storage.
-		 *
-		 * @param postId
-		 * @param type
-		 * @param body
-		 * @example cy.updatePageOrPostByRequest('12', { size: '20px' })
-		 */
-		updatePageOrPostByRequest(postId: string, type: string, body): Chainable;
+    /**
+     * Updates a post or page by request to WP Rest API.
+     * Needs a valid JWT set on local storage.
+     *
+     * @param postId
+     * @param type
+     * @param body
+     * @example cy.updatePageOrPostByRequest('12', { size: '20px' })
+     */
+    updatePageOrPostByRequest(postId: string, type: string, body): Chainable;
 
     /**
      * Creates a new tag with the desired name, and set the ID on local storage.
-     * 
-     * @param tagName 
+     *
+     * @param tagName
      * @example cy.createTagWithRequest('test-tag')
      */
     createTagWithRequest(tagName: string): Chainable;
-	}
+  }
 }
--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ Cypress.Commands.add('loginWithRequest', (nextRoute = '/wp-admin') => {
         body: {
           log: Cypress.env('user'),
           pwd: Cypress.env('password'),
-          'wp-submit': 'Log In'
+          'wp-submit': 'Log In',
         },
       });
     }
@@ -25,230 +25,235 @@ Cypress.Commands.add('loginWithRequest', (nextRoute = '/wp-admin') => {
 });
 
 Cypress.Commands.add('login', (nextRoute = '/wp-admin') => {
-	cy.getCookies({
-		log: true,
-	}).then((cookies) => {
-		let isLoggedIn = false;
-		cookies.forEach((value) => {
-			if (value.name.includes('wordpress_')) {
-				isLoggedIn = true;
-			}
-		});
+  cy.getCookies({
+    log: true,
+  }).then((cookies) => {
+    let isLoggedIn = false;
+    cookies.forEach((value) => {
+      if (value.name.includes('wordpress_')) {
+        isLoggedIn = true;
+      }
+    });
 
-		if (isLoggedIn) {
-				cy.visit(nextRoute);
-			return;
-		}
-		cy.visit('/wp-admin');
-		cy.wait(500);
-		cy.get('#user_login').type(Cypress.env('user'));
-		cy.get('#user_pass').type(Cypress.env('password'));
-		cy.get('#wp-submit').click();
-		cy.visit(nextRoute);
-	});
+    if (isLoggedIn) {
+      cy.visit(nextRoute);
+      return;
+    }
+    cy.visit('/wp-admin');
+    cy.wait(500);
+    cy.get('#user_login').type(Cypress.env('user'));
+    cy.get('#user_pass').type(Cypress.env('password'));
+    cy.get('#wp-submit').click();
+    cy.visit(nextRoute);
+  });
 });
 
 Cypress.Commands.add('clearWelcome', () => {
-	cy.window().then((win) => {
-		win.wp &&
-			win.wp.data &&
-			win.wp.data.select('core/edit-post').isFeatureActive('welcomeGuide') &&
-			win.wp.data.dispatch('core/edit-post').toggleFeature('welcomeGuide');
-	});
+  cy.window().then((win) => {
+    // eslint-disable-next-line chai-friendly/no-unused-expressions
+    win.wp &&
+      win.wp.data &&
+      win.wp.data.select('core/edit-post').isFeatureActive('welcomeGuide') &&
+      win.wp.data.dispatch('core/edit-post').toggleFeature('welcomeGuide');
+  });
 });
 
 Cypress.Commands.add(
-	'insertPost',
-	(title = 'Test', content = 'Content', type = 'post', featured = false, tags = false) => {
-		let loginRoute = '/wp-admin/post-new.php';
-		if (type !== 'post') {
-			loginRoute += '?post_type=' + type;
-		}
+  'insertPost',
+  (title = 'Test', content = 'Content', type = 'post', featured = false, tags = false) => {
+    let loginRoute = '/wp-admin/post-new.php';
+    if (type !== 'post') {
+      loginRoute += '?post_type=' + type;
+    }
 
-		cy.loginWithRequest(loginRoute);
-		cy.clearWelcome();
-		if (featured) {
-			cy.wait(1000);
-			cy.get('button').contains('Featured image').click();
-			cy.get('.editor-post-featured-image__toggle').click();
-			cy.get('.media-frame').find('.media-menu-item').contains('Media Library').click({
-				force: true,
-			});
+    cy.loginWithRequest(loginRoute);
+    cy.clearWelcome();
+    if (featured) {
+      cy.wait(1000);
+      cy.get('button').contains('Featured image').click();
+      cy.get('.editor-post-featured-image__toggle').click();
+      cy.get('.media-frame').find('.media-menu-item').contains('Media Library').click({
+        force: true,
+      });
 
-			cy.get('.attachments-browser .attachments > li.attachment').first().click({
-				force: true,
-			});
-			cy.get('.media-button-select').click();
-		}
+      cy.get('.attachments-browser .attachments > li.attachment').first().click({
+        force: true,
+      });
+      cy.get('.media-button-select').click();
+    }
 
-		if (tags) {
-			cy.get('.components-panel__body-toggle').contains('Tags').click({ force: true });
-			cy.get('.components-form-token-field__label')
-				.contains('Add New Tag')
-				.parent()
-				.find('input')
-				.type('test-tag,', { force: true });
-		}
-		cy.get('.editor-post-title__input').type(title);
-		cy.get(' textarea.block-editor-default-block-appender__content').click({
-			force: true,
-		});
-		cy.get('.block-editor-rich-text__editable').type(content);
-		cy.get('.editor-post-publish-panel__toggle').click();
-		cy.updatePost();
-	},
+    if (tags) {
+      cy.get('.components-panel__body-toggle').contains('Tags').click({ force: true });
+      cy.get('.components-form-token-field__label')
+        .contains('Add New Tag')
+        .parent()
+        .find('input')
+        .type('test-tag,', { force: true });
+    }
+    cy.get('.editor-post-title__input').type(title);
+    cy.get(' textarea.block-editor-default-block-appender__content').click({
+      force: true,
+    });
+    cy.get('.block-editor-rich-text__editable').type(content);
+    cy.get('.editor-post-publish-panel__toggle').click();
+    cy.updatePost();
+  },
 );
 
 Cypress.Commands.add(
-	'insertPostWithRequest',
-	(title = 'Test Title', content = 'Test content', type = 'posts', featured=0) => {
-		cy.loginWithRequest();
-		cy.getJWT().then(() => {
-			cy.request({
-				method: 'POST',
-				url: '/wp-json/wp/v2/' + type,
-				auth: {
-					bearer: window.localStorage.getItem('jwt'),
-				},
-				body: {
-					title,
-					status: 'publish',
-					content: {
-						raw: content,
-						rendered: 'The test content',
-						protected: false,
-					},
-					featured_media: featured
-				},
-			}).then((resp) => {
-				expect(resp.status).to.eq(201);
-				window.localStorage.setItem('postId', resp.body.id);
-				window.localStorage.setItem('postUrl', '/' + resp.body.slug);
-			});
-		});
-	},
+  'insertPostWithRequest',
+  (title = 'Test Title', content = 'Test content', type = 'posts', featured = 0) => {
+    cy.loginWithRequest();
+    cy.getJWT().then(() => {
+      cy.request({
+        method: 'POST',
+        url: '/wp-json/wp/v2/' + type,
+        auth: {
+          bearer: window.localStorage.getItem('jwt'),
+        },
+        body: {
+          title,
+          status: 'publish',
+          content: {
+            raw: content,
+            rendered: 'The test content',
+            protected: false,
+          },
+          featured_media: featured,
+        },
+      }).then((resp) => {
+        expect(resp.status).to.eq(201);
+        window.localStorage.setItem('postId', resp.body.id);
+        window.localStorage.setItem('postUrl', '/' + resp.body.slug);
+      });
+    });
+  },
 );
 
 Cypress.Commands.add('updatePost', () => {
-	cy.get('.editor-post-publish-button').click();
-	cy.wait(500);
+  cy.get('.editor-post-publish-button').click();
+  cy.wait(500);
 });
 
 Cypress.Commands.add('getCustomizerControl', (slug) => {
-	cy.window().then((win) => {
-		win.wp.customize.control(slug).focus();
-	});
-	return cy.get('#customize-control-' + slug);
+  cy.window().then((win) => {
+    win.wp.customize.control(slug).focus();
+  });
+  return cy.get('#customize-control-' + slug);
 });
 
 Cypress.Commands.add('setCustomizeSettings', (to) => {
-	cy.request('POST', '/wp-json/wpthememods/v1/settings', to);
+  cy.request('POST', '/wp-json/wpthememods/v1/settings', to);
 });
 
 Cypress.Commands.add('goToCustomizer', () => {
-	cy.loginWithRequest('/wp-admin/customize.php');
-	cy.visit('/wp-admin/customize.php');
-	cy.window()
-		.then((win) => {
-			//If the customizer is not ready, bind the flag to ready event.
-			win.wp.customize.bind('ready', () => {
-				win.appReady = true;
-			});
-		})
-		.then(() => {
-			// If we bind to the ready event too late, we can check the body class 'ready'.
-			cy.get('body').then(($body) => {
-				if ($body.hasClass('ready')) {
-					cy.window().then((win) => {
-						win.appReady = true;
-					});
-				}
-			});
-		});
-	cy.window({
-		timeout: 15000,
-	}).should('have.property', 'appReady', true);
+  cy.loginWithRequest('/wp-admin/customize.php');
+  cy.visit('/wp-admin/customize.php');
+  cy.window()
+    .then((win) => {
+      //If the customizer is not ready, bind the flag to ready event.
+      win.wp.customize.bind('ready', () => {
+        win.appReady = true;
+      });
+    })
+    .then(() => {
+      // If we bind to the ready event too late, we can check the body class 'ready'.
+      cy.get('body').then(($body) => {
+        if ($body.hasClass('ready')) {
+          cy.window().then((win) => {
+            win.appReady = true;
+          });
+        }
+      });
+    });
+  cy.window({
+    timeout: 15000,
+  }).should('have.property', 'appReady', true);
 });
 
 Cypress.Commands.add('aliasRestRoutes', () => {
-	cy.intercept('POST', '/wp-admin/admin-ajax.php').as('customizerSave');
+  cy.intercept('POST', '/wp-admin/admin-ajax.php').as('customizerSave');
 });
 
 Cypress.Commands.add('toggleElements', (show) => {
-	const icon = show ? 'dashicons-hidden' : 'dashicons-visibility';
-	cy.get('.ti-sortable-item-area .ti-sortable-item-toggle').each(function (el) {
-		cy.get(el)
-			.find('.dashicon')
-			.then(($icon) => {
-				if ($icon.hasClass(icon)) {
-					cy.get($icon).click();
-				}
-			});
-	});
+  const icon = show ? 'dashicons-hidden' : 'dashicons-visibility';
+  cy.get('.ti-sortable-item-area .ti-sortable-item-toggle').each(function (el) {
+    cy.get(el)
+      .find('.dashicon')
+      .then(($icon) => {
+        if ($icon.hasClass(icon)) {
+          cy.get($icon).click();
+        }
+      });
+  });
 });
 
 Cypress.Commands.add('getControl', (control) => {
-	return cy.get(`label[for=${control}]`).parent();
+  return cy.get(`label[for=${control}]`).parent();
 });
 
 Cypress.Commands.add('activateCheckbox', (checkboxSelector, checkboxText) => {
-	cy.get(checkboxSelector)
-		.contains(checkboxText)
-		.prev()
-		.then((checkbox) => {
-			if (!checkbox.hasClass('is-checked')) {
-				cy.get(checkbox).click();
-			}
-		});
+  cy.get(checkboxSelector)
+    .contains(checkboxText)
+    .prev()
+    .then((checkbox) => {
+      if (!checkbox.hasClass('is-checked')) {
+        cy.get(checkbox).click();
+      }
+    });
 });
 
 Cypress.Commands.add('openNeveSidebar', () => {
-	cy.get('button.components-button[aria-label="Neve Options"]').click();
+  cy.get('button.components-button[aria-label="Neve Options"]').click();
 });
 
 Cypress.Commands.add('activateClassicEditorPlugin', () => {
-	cy.exec('docker-compose -f ../docker-compose.ci.yml run --rm -u root cli wp --allow-root plugin activate classic-editor')
+  cy.exec(
+    'docker-compose -f ../docker-compose.ci.yml run --rm -u root cli wp --allow-root plugin activate classic-editor',
+  );
 });
 
 Cypress.Commands.add('deactivateClassicEditorPlugin', () => {
-	cy.exec('docker-compose -f ../docker-compose.ci.yml run --rm -u root cli wp --allow-root plugin deactivate classic-editor')
+  cy.exec(
+    'docker-compose -f ../docker-compose.ci.yml run --rm -u root cli wp --allow-root plugin deactivate classic-editor',
+  );
 });
 
 Cypress.Commands.add('getJWT', () => {
-	cy.request('POST', '/wp-json/api-bearer-auth/v1/login', {
-		username: 'admin',
-		password: 'admin',
-	}).then((response) => {
-		expect(response.body.access_token).to.exist;
-		window.localStorage.setItem('jwt', response.body.access_token);
-	});
+  cy.request('POST', '/wp-json/api-bearer-auth/v1/login', {
+    username: 'admin',
+    password: 'admin',
+  }).then((response) => {
+    expect(response.body.access_token).to.exist;
+    window.localStorage.setItem('jwt', response.body.access_token);
+  });
 });
 
 Cypress.Commands.add('updatePageOrPostByRequest', (postId, type, body) => {
-	cy.getJWT().then(() => {
-		cy.request({
-			method: 'POST',
-			url: '/wp-json/wp/v2/' + type + '/' + postId,
-			auth: {
-				bearer: window.localStorage.getItem('jwt'),
-			},
-			body,
-		});
-	});
+  cy.getJWT().then(() => {
+    cy.request({
+      method: 'POST',
+      url: '/wp-json/wp/v2/' + type + '/' + postId,
+      auth: {
+        bearer: window.localStorage.getItem('jwt'),
+      },
+      body,
+    });
+  });
 });
 
 Cypress.Commands.add('createTagWithRequest', (tagName) => {
-  cy.getJWT().then(() =>{
+  cy.getJWT().then(() => {
     cy.request({
       method: 'POST',
       url: '/wp-json/wp/v2/tags',
       auth: {
-        bearer: window.localStorage.getItem('jwt')
+        bearer: window.localStorage.getItem('jwt'),
       },
-      body: {name: tagName}
+      body: { name: tagName },
     }).then((resp) => {
       expect(resp.status).to.eq(201);
       window.localStorage.setItem('tagId', resp.body.id);
-    })
-  })
-})
+    });
+  });
+});
